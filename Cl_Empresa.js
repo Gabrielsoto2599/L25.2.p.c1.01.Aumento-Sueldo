a674927b694da7e466ec9fa9b7de0e14a9b2de59
Gabrielsoto2599 <135544457+Gabrielsoto2599@users.noreply.github.com>
2026-04-03 12:40:51 -0400

    Cl_Empresa.js

--- Cl_Empresa.js
+++ Cl_Empresa.js
@@ -1,26 +1,15 @@
-export default class Cl_Empleado {
-  constructor(nombre, rol, sueldoActual) {
-    this.nombre = nombre;
-    this.rol = rol; // 'Senior', 'Mid', 'Junior', 'DevOps'
-    this.sueldoActual = sueldoActual;
+export default class Cl_Empresa {
+  constructor() {
+    this.acumSueldosNuevos = 0;
+    this.contEmpleados = 0;
   }
 
-  // Lógica de aumento según el rol
-  porcentajeAumento() {
-    switch (this.rol.toLowerCase()) {
-      case 'junior': return 20; // 20% de aumento
-      case 'mid': return 15; // 15% de aumento
-      case 'devops': return 12; // 12% de aumento
-      case 'senior': return 10; // 10% de aumento
-      default: return 5;
-    }
+  procesarEmpleado(e) {
+    this.contEmpleados++;
+    this.acumSueldosNuevos += e.nuevoSueldo();
   }
 
-  montoAumento() {
-    return this.sueldoActual * (this.porcentajeAumento() / 100);
-  }
-
-  nuevoSueldo() {
-    return this.sueldoActual + this.montoAumento();
+  totalNominaMensual() {
+    return this.acumSueldosNuevos;
   }
 }
